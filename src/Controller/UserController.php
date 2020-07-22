26abe99891f964b2ece2239104d62961bf4392e3
Rémi Zanzi <r.zanzi@imanaging-document.fr>
2020-07-22 17:26:18 +0200

    Ajout d'une méthode de controller

---
+++ src/Controller/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Imanaging\CoreApplicationBundle\Controller;
+
+use Imanaging\CoreApplicationBundle\CoreApplication;
+use Imanaging\ZeusUserBundle\Interfaces\UserInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends AbstractController
+{
+  private $em;
+  private $coreApplication;
+
+  /**
+   * MappingController constructor.
+   * @param EntityManagerInterface $em
+   * @param CoreApplication $coreApplication
+   */
+  public function __construct(EntityManagerInterface $em, CoreApplication $coreApplication)
+  {
+    $this->em = $em;
+    $this->coreApplication = $coreApplication;
+  }
+
+  public function indexAction(Request $request)
+  {
+    $params = $request->request->all();
+    return $this->render("@ImanagingCoreApplication/User/index.html.twig", [
+      'utilisateurs' => $utilisateurs = $this->em->getRepository(UserInterface::class)->findAll(),
+      'basePath' => $params['basePath']
+    ]);
+  }
+}
